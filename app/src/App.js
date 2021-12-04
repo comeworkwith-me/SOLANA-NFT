7898bff78629ce888dd8add5a4fd83959d414de6
Takeira <walls.takeira@gmail.com>
2021-12-04 18:39:48 -0500

    I added wallet functionality for Phantom.

--- app/src/App.js
+++ app/src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import './App.css';
 import twitterLogo from './assets/twitter-logo.svg';
 
@@ -7,6 +7,48 @@ const TWITTER_HANDLE = '_buildspace';
 const TWITTER_LINK = `https://twitter.com/${TWITTER_HANDLE}`;
 
 const App = () => {
+  // FUNCTIONS
+
+  // This will check if the user has a Solana object, if the Solana object is 
+  // a Phantom wallet, and if there's authorization to the Phantom wallet.
+  const walletConnected = async () => {
+    try {
+      const { solana } = window;
+
+      if (solana) {
+        if (solana.isPhantom) {
+          console.log("Phantom wallet is found!");
+
+          const response = await solana.connect({ onlyIfTrusted: true });
+          console.log(
+            "Connected with Public Key:",
+            response.publicKey.toString()
+          );
+        }
+      } else {
+        alert("Solana object is not found! Get a phantom wallet 👻");
+      }
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  // USEEFFECTS
+
+  // This will check if there's a connected phantom wallet as soon as the 
+  // component mounts.
+  useEffect(() => {
+    const onLoad = async () => {
+      await walletConnected();
+    };
+    window.addEventListener("load", onLoad);
+    return () => {
+      window.removeEventListener("load", onLoad);
+    }
+  }, []);
+
+
+
   return (
     <div className="App">
       <div className="container">
